cfd4f295e259a61cafda4b225947253a6c4326e7
Lenovo <Lenovo@DESKTOP-8HSBAI9>
2024-06-25 19:00:00 +0530

    Encapsulation

---
+++ Batch96/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,14 @@
+//Program to demonstrate Encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum;//Data Variables
+	String name;
+	int age;
+	
+	void show()//Member Function
+	{
+		System.out.println("serialNum"+serialNum+"\t name"+name+"\t age"+age);
+	}
+
+}
---
+++ Batch96/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+	
+	public static void main(String[] agrs) {
+		//TODO Auto-generated method sLub
+		
+		Encapsulation ob= new Encapsulation();//object creation
+		ob.name="suma";//calling the variables
+		ob.age=33;
+		ob.serialNum=22;
+		ob.show();
+	}
+
+}
